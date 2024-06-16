71035fdf9fcd129013e78b1a9927f6481377b1a1
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-16 16:19:50 +0200

    DatePicker created

--- src/components/Layout/AddAssetForm.jsx
+++ src/components/Layout/AddAssetForm.jsx
@@ -1,4 +1,4 @@
-import { Flex, Select, Space, Typography, Divider } from "antd";
+import { Flex, Select, Space, Typography, Divider, Form, Button, InputNumber, DatePicker } from "antd";
 import { useState } from "react";
 import { useCrypto } from "../../context/crypto-context";
 
@@ -29,13 +29,73 @@ function AddAssetForm() {
         )
     }
 
-    return <form>
+    function onFinish(values) {
+        
+    }
+
+    return <Form
+            name="basic"
+            labelCol={{
+            span: 4,
+            }}
+            wrapperCol={{
+            span: 10,
+            }}
+            style={{
+            maxWidth: 600,
+            }}
+            initialValues={{}}
+            onFinish={onFinish}
+        >
         <Flex align="center">
                 <img src={coin.icon} alt={coin.name} style={{ width: 40, marginRight: 10 }} />
                 <Typography.Title level={2} style={{ margin: 0 }}>{coin.name}</Typography.Title>
         </Flex>
         <Divider />
-    </form>
+        
+    <Form.Item
+      label="Amount"
+      name="amount"
+      rules={[
+        {
+            required: true,
+            type: 'number',
+            min: 0,
+            message: 'Please input your username!',
+        },
+      ]}
+    >
+      <InputNumber style={{ width: '100%' }} />
+    </Form.Item>
+
+    <Form.Item
+      label="Price"
+      name="price"
+    >
+      <InputNumber disabled style={{ width: '100%' }} />
+    </Form.Item>
+        
+    <Form.Item
+      label="Date & Time"
+      name="date"
+    >
+      <DatePicker showTime style={{ width: '100%' }} />
+    </Form.Item>
+
+
+    <Form.Item
+      label="Total"
+      name="total"
+    >
+      <InputNumber disabled style={{ width: '100%' }} />
+    </Form.Item>
+
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Add Asset
+      </Button>
+    </Form.Item>
+  </Form>
 }
 
 export default AddAssetForm;
